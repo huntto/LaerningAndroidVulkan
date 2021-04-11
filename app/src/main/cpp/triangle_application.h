0153f2e7673a74214938710c69bbe99f8f5c8aa5
Xiang Tao <huntto1990@icloud.com>
2021-04-11 17:24:52 +0800

    Add vertex

--- app/src/main/cpp/triangle_application.h
+++ app/src/main/cpp/triangle_application.h
@@ -4,6 +4,37 @@
 #include "tiny_engine/vulkan_application.h"
 
 #include <vulkan/vulkan_android.h>
+#include <glm/glm.hpp>
+
+struct Vertex {
+    glm::vec3 pos;
+    glm::vec3 color;
+
+    static std::vector<VkVertexInputBindingDescription> GetBindingDescription() {
+        VkVertexInputBindingDescription binding_description{};
+        binding_description.binding = 0;
+        binding_description.stride = sizeof(Vertex);
+        binding_description.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+        return {binding_description};
+    }
+
+    static std::vector<VkVertexInputAttributeDescription> GetAttributeDescriptions() {
+        std::vector<VkVertexInputAttributeDescription> attribute_descriptions(2);
+        attribute_descriptions[0].binding = 0;
+        attribute_descriptions[0].location = 0;
+        attribute_descriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attribute_descriptions[0].offset = offsetof(Vertex, pos);
+        attribute_descriptions[1].binding = 0;
+        attribute_descriptions[1].location = 1;
+        attribute_descriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attribute_descriptions[1].offset = offsetof(Vertex, color);
+        return attribute_descriptions;
+    }
+
+    bool operator==(const Vertex &other) const {
+        return pos == other.pos && color == other.color;
+    }
+};
 
 class TriangleApplication : public tiny_engine::VulkanApplication {
 public:
@@ -22,6 +53,8 @@ public:
         native_window_ = native_window;
         vert_shader_code_ = vert_shader_code;
         frag_shader_code_ = frag_shader_code;
+        binding_descriptions_ = Vertex::GetBindingDescription();
+        attribute_descriptions_ = Vertex::GetAttributeDescriptions();
     }
 };
 
